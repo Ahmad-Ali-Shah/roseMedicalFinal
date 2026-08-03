ebc7a27d0137a656773da7056024c5c1a3d63748
manbtd0-cloud <manbtd0@gmail.com>
2026-08-03 22:21:03 +0500

    feat: stagger procurement information checklist

--- apps/web/src/features/procurement-support/information-checklist.tsx
+++ apps/web/src/features/procurement-support/information-checklist.tsx
@@ -1,15 +1,16 @@
 import type { ReactElement } from "react";
+import { Stagger, StaggerItem } from "@/features/motion";
 import { INFORMATION_CHECKLIST } from "./procurement-support.data";
 
 export function InformationChecklist(): ReactElement {
   return (
-    <ol className="information-checklist" aria-label="Information that helps">
+    <Stagger as="ol" className="information-checklist" aria-label="Information that helps" interval={0.05}>
       {INFORMATION_CHECKLIST.map((item, index) => (
-        <li key={item} data-information-item={item}>
+        <StaggerItem as="li" key={item} data-information-item={item}>
           <span aria-hidden="true">{String(index + 1).padStart(2, "0")}</span>
           <strong>{item}</strong>
-        </li>
+        </StaggerItem>
       ))}
-    </ol>
+    </Stagger>
   );
 }
