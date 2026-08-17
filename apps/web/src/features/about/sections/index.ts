18b5cf659cf6304c91e436a70d2d1a2452d63c47
manbtd0-cloud <manbtd0@gmail.com>
2026-08-17 09:36:55 -0700

    fix(web): export About redesign sections

---
+++ apps/web/src/features/about/sections/index.ts
@@ -0,0 +1,8 @@
+export { AboutCompactHero } from "./about-compact-hero";
+export { AboutCompliance } from "./about-compliance";
+export { AboutContactBand } from "./about-contact-band";
+export { AboutDocuments } from "./about-documents";
+export { AboutIntroduction } from "./about-introduction";
+export { AboutQuotationCta } from "./about-quotation-cta";
+export { AboutSocialStrip } from "./about-social-strip";
+export { AboutStorySection } from "./about-story-section";
